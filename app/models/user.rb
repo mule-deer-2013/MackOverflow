0eaecfd999eb295fcd170adc5f6fdbc3e29647f9
audibleblink <virtualsnyper@gmail.com>
2013-09-18 15:11:45 -0700

    created first fail test

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,8 @@
 class User < ActiveRecord::Base
+
+  attr_accessible :username
   has_many :questions #alias this
   has_many :answers #alias this
+
 
 end
---
+++ spec/models/user_spec.rb
@@ -0,0 +1,16 @@
+require 'spec_helper'
+
+describe User do
+
+  let(:user) { User.new(username: username) } 
+  let(:username) { "Test User" }
+
+
+  it "should be able to create an account" do
+    user.save
+    User.last.username.should eq "Test User"
+  end
+
+  it "should be able to log in"
+
+end
