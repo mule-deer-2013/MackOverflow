12468fb0fcfd5e55cc3806baf5c9e491893e9840
audibleblink <virtualsnyper@gmail.com>
2013-09-18 16:30:23 -0700

    more user test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,20 +2,23 @@ require 'spec_helper'
 
 describe User do
 
-  let(:user) { User.new(username: username) } 
   let(:username) { "Test User" }
+  let(:password) { "alex" }
+  let(:user) { User.new(username: username, password: password) } 
 
 
   it "should be able to create an account" do
     user.save
-    User.last.username.should eq "Test User"
+    User.last.username.should eq("Test User")
   end
 
-  it "should be able to log in" do 
-    pending
+  it "should be able to log in with a correct p/w" do 
+    user.save
+    expect ( 
+      user.try(:authenticate, password) 
+    ).should be_kind_of User
   end
-
-
+  
 end
 
 
