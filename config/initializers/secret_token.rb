f81ccde9a425ff61f53a46adb5bafe23ed4095e0
audibleblink <virtualsnyper@gmail.com>
2013-09-18 14:33:44 -0700

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MackOverflow::Application.config.secret_token = 'cf6936874bdf0564e9194ef96bc44398d3278e234c2e6d0376950b0d29efe5f6ba8e9820ff97ad1e36b7dbd93825409ade394924cc116ad6d503297a1ef7653b'
