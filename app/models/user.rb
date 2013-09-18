cc711c2ad4ea32ea01e08d916b922395c62aa13c
audibleblink <virtualsnyper@gmail.com>
2013-09-18 15:55:29 -0700

    used has_secure_password to install bcrypt and added password_digest to User migration

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,33 +1,29 @@
 class User < ActiveRecord::Base
 
+has_secure_password
+
   attr_accessible :username, :password
   has_many :questions #alias this
   has_many :answers #alias this
 
 
-  include BCrypt
+  # include BCrypt
 
-  def password
-    @password ||= Password.new(password_hash)
-  end
+  # def password
+  #   @password ||= Password.new(password_hash)
+  # end
 
-  def password=(new_password)
-    @password = Password.create(new_password)
-    self.password_hash = @password
-  end
-
-  def login(username, pass)
-  @user = User.find_by_username(username)
-    if @user.password == pass
-      give_token
-    else
-      redirect_to home_url
-    end
-  end
+  # def password=(new_password)
+  #   @password = Password.create(new_password)
+  #   self.password_hash = @password
+  # end
 
 
-  def give_token
+
+
+
+  # def give_token
     
-  end
+  # end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130918175458) do
+ActiveRecord::Schema.define(:version => 20130918225329) do
 
   create_table "answers", :force => true do |t|
     t.integer  "rating"
@@ -33,8 +33,9 @@ ActiveRecord::Schema.define(:version => 20130918175458) do
   create_table "users", :force => true do |t|
     t.string   "username"
     t.string   "password_hash"
-    t.datetime "created_at",    :null => false
-    t.datetime "updated_at",    :null => false
+    t.datetime "created_at",      :null => false
+    t.datetime "updated_at",      :null => false
+    t.string   "password_digest"
   end
 
 end
