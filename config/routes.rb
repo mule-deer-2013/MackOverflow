2b698921359100b025aedac727670499a6119d7f
audibleblink <virtualsnyper@gmail.com>
2013-09-19 09:27:57 -0700

    merge

--- config/routes.rb
+++ config/routes.rb
@@ -16,9 +16,7 @@ MackOverflow::Application.routes.draw do
   resources :questions
   resources :answers, except: [:index]
 
-<<<<<<< HEAD
- root :to => 'questions#index'
-=======
+
   resources :users do 
     resources :questions, :only => [:show]
   end
@@ -28,6 +26,5 @@ MackOverflow::Application.routes.draw do
   end
 
  root :to => 'question#index'
->>>>>>> 2b9f9cd32c0d0bce86aac5c105d6e86a0b0f751b
 
 end
