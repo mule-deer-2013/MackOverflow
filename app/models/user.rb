7a9c28bc108c76b8e2066ef81f7d1be52ab5b4f8
audibleblink <virtualsnyper@gmail.com>
2013-09-18 17:42:31 -0700

    added user controller methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,29 +1,11 @@
 class User < ActiveRecord::Base
 
-has_secure_password
+  has_secure_password
+  
+  validates_presence_of :password, :on => :create
 
   attr_accessible :username, :password
   has_many :questions #alias this
   has_many :answers #alias this
-
-
-  # include BCrypt
-
-  # def password
-  #   @password ||= Password.new(password_hash)
-  # end
-
-  # def password=(new_password)
-  #   @password = Password.create(new_password)
-  #   self.password_hash = @password
-  # end
-
-
-
-
-
-  # def give_token
-    
-  # end
 
 end
