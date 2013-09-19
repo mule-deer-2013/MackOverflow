ac8995ad63a192c15e2ba8c87c1860a2ca040942
audibleblink <virtualsnyper@gmail.com>
2013-09-18 17:00:08 -0700

    controllers added and hlper methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,18 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  helper_method: :current_user, :logged_in?, :logout
+
+  def current_user
+    @current_user ||= User.find_by_id!(session[:user_id])
+  end
+
+  def logged_in?
+    current_user != nil
+  end
+
+  def logout
+    session.clear
+  end
+
+
 end
