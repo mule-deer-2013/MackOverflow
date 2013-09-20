7ae8282886d87aa7b0f669cec414d1fad31dc9a5
audibleblink <virtualsnyper@gmail.com>
2013-09-20 14:42:50 -0700

    corrected models

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -2,7 +2,7 @@ class Answer < ActiveRecord::Base
   attr_accessible :content
   belongs_to :user
   belongs_to :question
-  has_many :votes as: :votable
+  has_many :votes, as: :votable
 
   validates_presence_of :content
 
--- app/models/question.rb
+++ app/models/question.rb
@@ -2,7 +2,7 @@ class Question < ActiveRecord::Base
   attr_accessible :content
   belongs_to :user
   has_many :answers
-  has_many :votes as: :votable
+  has_many :votes, as: :votable
 
   validates_presence_of :content
 end
