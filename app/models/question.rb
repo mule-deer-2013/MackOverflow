181d20137c7677bb36304436e09ef4b5c79f606b
Christian Joseph <christianrjo@gmail.com>
2013-09-23 18:21:39 -0700

    fix tagging system, now you can collect all

--- app/models/question.rb
+++ app/models/question.rb
@@ -24,7 +24,7 @@ class Question < ActiveRecord::Base
 
   def tag_list=(tags_string)
     tag_names = tags_string.split(",").collect{|s| s.strip.downcase}.uniq
-    new_or_found_tags = tag_names.collect { |name| Tag.create(name: name) }
+    new_or_found_tags = tag_names.collect { |name| Tag.find_or_create_by_name(name) }
     self.tags = new_or_found_tags
   end
 
